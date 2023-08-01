b0c6d3ac0cb25dff232708039f774aa170392960
Crocus98 <matteosavino98@gmail.com>
2023-08-01 14:24:49 +0200

    Contract setup

--- SmartContracts/DataContract.sol
+++ SmartContracts/DataContract.sol
@@ -1,5 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Test{
-    
+//Contract that gives data about trains
+contract TrainsOracle{
+    address payable public trainCompany;
+    string public trainCompanyName = "ItalyTrains";
+
+    struct Trip{
+        uint _trainId;
+        uint _tripId;
+        uint _stepId;
+        address _sender;
+    }
+    //mapping(Trip => uint) private trips;
+
+    constructor() {
+        trainCompany = payable(msg.sender);
+    }
+
+    event TrainArrivalEvent(
+        uint _trainId,
+        uint _tripId,
+        uint _stepId,
+        address _sender
+    );
+
+
 }
