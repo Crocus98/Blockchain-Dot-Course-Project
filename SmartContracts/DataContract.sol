16dde060ef4b22e31a1c946ff7c3087c0f1b1aec
Crocus98 <matteosavino98@gmail.com>
2023-08-01 18:53:13 +0200

    Contract cont'd

--- SmartContracts/DataContract.sol
+++ SmartContracts/DataContract.sol
@@ -6,13 +6,46 @@ contract TrainsOracle{
     address payable public trainCompany;
     string public trainCompanyName = "ItalyTrains";
 
-    struct Trip{
-        uint _trainId;
-        uint _tripId;
-        uint _stepId;
-        address _sender;
+    //event mttq o signalR
+    struct Station {
+        string _stationName;
+        bool _stationState;
     }
-    //mapping(Trip => uint) private trips;
+    mapping (uint32 => Station) stations;
+
+    struct Train {
+        string _trainName;
+        uint16 _passengersNumber;
+        bool _trainState;
+        uint32 [] _stationsId;
+    }
+    mapping (uint32 => Train) trains;
+
+    struct Segment {
+        uint32 _trainId;
+
+        uint32 _startingStationId;
+        uint32 _arrivingStationId;
+
+        uint32 _segmentPrice;
+
+        uint256 _segmentStartingTime;
+        uint256 _segmentEndingTime;
+    }
+    mapping (uint256 => Segment) segments;
+
+    struct Ticket {
+        uint256 _segmentId;
+        uint256 _segmentActualEndingTime;
+        bool valid;
+    }
+    mapping (uint256 => Ticket) tickets;
+
+    struct Trip {
+        uint256 [] _ticketIds;
+    }
+    mapping (address=>Trip) trips;
+
 
     constructor() {
         trainCompany = payable(msg.sender);
