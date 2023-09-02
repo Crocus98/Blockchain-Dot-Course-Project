b1b8d1b657886cc9cd36001bf8fe810de42edcda
Matteo Savino <matteosavino98@gmail.com>
2023-09-02 22:35:37 +0200

    Small fix

--- Python/Utility/SmartContractUtility.py
+++ Python/Utility/SmartContractUtility.py
@@ -2,6 +2,8 @@ from web3 import Web3
 from solcx import compile_standard
 import json
 from dotenv import dotenv_values, set_key
+
+
 class SmartContractUtility:
 
     @staticmethod
@@ -10,8 +12,9 @@ class SmartContractUtility:
 
     @staticmethod
     def compile_contract(contract_source_path, contract_abi_path, contract_bytecode_path, contract_name):
-        contract_source_code = SmartContractUtility.get_contract_source_code(contract_source_path)
-        
+        contract_source_code = SmartContractUtility.get_contract_source_code(
+            contract_source_path)
+
         compiled_contract = compile_standard({
             "language": "Solidity",
             "sources": {
@@ -27,13 +30,14 @@ class SmartContractUtility:
                 },
                 "optimizer": {
                     "enabled": True,
-                    "runs": 200    
+                    "runs": 200
                 },
             },
         })
-        
+
         contract_abi = compiled_contract["contracts"][contract_source_path][contract_name]["abi"]
-        contract_bytecode = compiled_contract["contracts"][contract_source_path][contract_name]["evm"]["bytecode"]["object"]
+        contract_bytecode = compiled_contract["contracts"][
+            contract_source_path][contract_name]["evm"]["bytecode"]["object"]
 
         # Save ABI to the specified path
         with open(contract_abi_path, "w") as abi_file:
@@ -42,21 +46,21 @@ class SmartContractUtility:
         # Save bytecode to the specified path
         with open(contract_bytecode_path, "w") as bytecode_file:
             bytecode_file.write(contract_bytecode)
-        
+
         return contract_source_code, contract_abi, contract_bytecode
-    
+
     @staticmethod
     def get_contract_abi(contract_abi_path):
         with open(contract_abi_path, "r") as abi_file:
             contract_abi = json.load(abi_file)
         return contract_abi
-    
+
     @staticmethod
     def get_contract_bytecode(contract_bytecode_path):
         with open(contract_bytecode_path, "r") as bytecode_file:
             contract_bytecode = bytecode_file.read()
         return contract_bytecode
-    
+
     @staticmethod
     def get_contract_instance(web3, contract_address, contract_abi):
         return web3.eth.contract(address=contract_address, abi=contract_abi)
@@ -68,22 +72,23 @@ class SmartContractUtility:
         return contract_source_code
 
     @staticmethod
-    def deploy_contract(web3, contract_abi, contract_bytecode, sender_private_key, value = None, gas_limit=None, gas_price=None):
+    def deploy_contract(web3, contract_abi, contract_bytecode, sender_private_key, value=None, gas_limit=None, gas_price=None):
         sender_account = web3.eth.account.from_key(str(sender_private_key))
         nonce = web3.eth.get_transaction_count(sender_account.address)
 
-        contract = web3.eth.contract(abi=contract_abi, bytecode=contract_bytecode)
+        contract = web3.eth.contract(
+            abi=contract_abi, bytecode=contract_bytecode)
         contract_constructor = contract.constructor()
-        
+
         if gas_price is None:
             gas_price = web3.eth.gas_price
-        
+
         if gas_limit is None:
-            gas_limit = 3000000 #contract_constructor.estimate_gas()
-        
+            gas_limit = 3000000  # contract_constructor.estimate_gas()
+
         if value is None:
             value = web3.to_wei(1, 'ether')
-        
+
         tx_params = {
             "nonce": nonce,
             "gas": gas_limit,
@@ -97,7 +102,7 @@ class SmartContractUtility:
 
         print("Contract address: " + tx_receipt.contractAddress)
         return tx_receipt.contractAddress
-    
+
     @staticmethod
     def set_contract_address_in_env(address, dotenv_path):
         env_file_path = dotenv_path
@@ -106,7 +111,7 @@ class SmartContractUtility:
         env_values["CONTRACTADDRESS"] = address
 
         set_key(env_file_path, "CONTRACTADDRESS", address)
-    
+
     @staticmethod
     def call_contract_function(web3, contract, function_name, function_params, sender_private_key, value=None, gas_limit=None, gas_price=None):
         sender_account = web3.eth.account.from_key(str(sender_private_key))
@@ -116,27 +121,24 @@ class SmartContractUtility:
             gas_price = web3.eth.gas_price
 
         if gas_limit is None:
-            gas_limit = contract.functions[function_name](*function_params).estimate_gas({'from': sender_account.address})
-        
+            gas_limit = contract.functions[function_name](
+                *function_params).estimate_gas({'from': sender_account.address})
+
         nonce = web3.eth.get_transaction_count(sender_account.address)
-        
+
         transaction_data = {
             'nonce': nonce,
             'gas': gas_limit,
             'gasPrice': gas_price,
         }
-        
+
         if value is not None:
             transaction_data['value'] = value
-        
-        transaction_data = function(*function_params).build_transaction(transaction_data)
-        
+
+        transaction_data = function(
+            *function_params).build_transaction(transaction_data)
+
         signed_txn = sender_account.sign_transaction(transaction_data)
         tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
 
         return tx_hash
-
-
-
-
-
